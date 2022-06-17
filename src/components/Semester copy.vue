58d5e7a1b2f43173b10df5c8449931c8df1a398b
Snrts <spm.aarts@student.maastrichtuniversity.nl>
2022-06-17 15:12:54 +0200

    Revert "backup"
    This reverts commit 33eff1af97b8a31251a1ff610d42a808d57c0768.

---
+++ src/components/Semester copy.vue
@@ -0,0 +1,124 @@
+<template>
+    <Toggle @togglePeriod="periodInSemester"  />
+    <div class="grid gap-4 grid-cols-3 grid-flow-row-dense  w-3/4 bg-white rounded-3xl truncate mx-auto p-8">
+        <div  v-for="(period, index) in periods" :key="index"  class="relative block h-fit rounded-3xl  w-full" :class="(period == 1) ? 'bg-gray-100  col-span-3  row-span-1'
+                                        :(period == 2) ? 'bg-gray-100 row-start-3 col-start-2 col-span-1 '
+                                        :(period == 4) ? 'bg-gray-100 row-start-3 col-start-1 col-span-1 row-span-1 '
+                                        : 'bg-gray-100 row-start-3 col-start-2 col-span-1 row-span-1 '">
+                <h2 class="text-black rounded-t-xl text-center tracking-widest">PERIOD {{ period }} {{ count }}</h2>
+        </div>  
+            <div  :class="(subject == 'PRA') ? 'col-span-3' : 'col-span-1'" class="bg-gray-100" v-for="(subject, index) in sub" :key="index">
+    
+                <button  class="h-12 m-3 rounded-full w-full text-center bg-gray-200 ">{{ subject }} </button>       
+        
+                <div class="grid grid-cols-4"> <button  v-for="module in modules" v-show="module.subject == subject" :key="module.id" class="relative block pr-70 bg-white text-start pr-8 rounded-full m-3 h-12 min-w-28 "
+                :class=" (module.subject === 'BIO') ? 'text-green-500  col-span-2 '
+                                : (module.subject === 'CHE') ? 'text-teal-500 col-span-2  '
+                                : (module.subject === 'PHY') ? 'text-sky-500 col-span-2  '
+                                : (module.subject === 'MAT') ? 'text-red-400 col-span-2 '
+                                : (module.subject === 'INT') ? 'text-indigo-400 col-span-2 '
+                                : (module.subject === 'NEU') ? 'text-rose-300 col-span-2 '
+                                :'bg-slate-600 col-span-1'" @click="makeChoice(module.id, module.subject, module.code, module.start1, module.end1, module.start2, module.end2, module.start3, module.end3)">
+                                <p class="font-bold mx-auto  text-xs inline"> {{module.subject}} {{module.code}} {{module.id}} </p>
+                                
+                                <span class="w-10 h-12 inline-block rounded-r-full -my-3 absolute right-0" :class="(module.subject === 'BIO') ? 'bg-green-500  col-span-1 row-span-1'
+                                : (module.subject === 'CHE') ? 'bg-teal-500'
+                                : (module.subject === 'PHY') ? 'bg-sky-500'
+                                : (module.subject === 'MAT') ? 'bg-red-400'
+                                : (module.subject === 'INT') ? 'bg-indigo-400'
+                                : (module.subject === 'NEU') ? 'bg-rose-300'
+                            : 'bg-slate-600'"> <a class="text-white text-bold justify-self-center absolute top-3" >i</a> </span> </button>
+            </div> 
+           </div>
+           
+         </div>
+        
+ 
+
+    
+    
+</template>
+
+
+<script>
+import Toggle from './Toggle.vue'
+import Choice from './Choice.vue'
+export default {
+    name: 'Semester',
+    props: {
+        index: Intl,
+        modules: Array,
+    },
+    components: {
+        Toggle,
+        Choice
+    },
+    data() {
+        return {
+            periods: 4,
+            sub: ['BIO', 'CHE', 'PHY', 'INT', 'NEU', 'MAT', 'PRA'],
+
+
+        }
+    },
+    methods: {
+        // periodInSemester(count) {
+        //     if (count % 2 !== 0) {
+        //         const a = [1]
+        //         this.periods = a
+
+        //     }
+        //     else {
+        //         const a = [4]
+        //         this.periods = a
+
+        //     }
+        // },
+        // periodInSemester(count) {
+        //     if (count % 2 !== 0) {
+        //         const a = [1]
+        //         this.period = a
+
+        //     }
+        //     else {
+        //         const a = [4]
+        //         this.period = a
+
+        //     }
+        // },
+
+        periodInSemester(count) {
+            switch (count) {
+                case 1:
+                    this.periods = 1;
+                    break;
+                case 2:
+                    this.periods = 2;
+                    break;
+                case 3:
+                    count = 4;
+                    break;
+                case 4:
+                    this.periods = 4;
+                    break;
+                case 5:
+                    this.periods = 5;
+                    break;
+                default:
+                    count = 1;
+                    this.periods = 1
+                    break;
+                
+            }
+        },
+
+        makeChoice(id, subject, code, start1, end1, start2, end2, start3, end3) {
+            this.$emit("makechoice", id, subject, code, start1, end1, start2, end2, start3, end3)
+        },
+
+
+
+    }
+}
+
+</script>
--- src/components/Semester.vue
+++ src/components/Semester.vue
@@ -1,38 +1,26 @@
 <template>
-    <div class="grid gap-4 grid-cols-2 grid-flow-row-dense  w-3/4 bg-white rounded-3xl truncate  p-8" >
-        <div class="relative block h-14 rounded-3xl w-full bg-gray-100  col-span-2  row-span-1">
-            <h2 class="text-black rounded-t-xl  text-center tracking-widest" >
-                PERIOD {{ p }} 
-            </h2>
+    <div class="grid gap-4 grid-cols-3 grid-flow-row-dense md:gridcols-2 w-3/4 bg-white rounded-3xl truncate  p-8" >
+        <div class="relative block h-14 rounded-3xl w-full bg-gray-100  col-span-3  row-span-1">
+                <h2 class="text-black rounded-t-xl  text-center tracking-widest" >PERIOD {{ p }} </h2>
         </div>  
-        <div 
-                :class="(subject == 'PRA') ? 'col-span-2' : 'col-span-1'" 
-                class="bg-gray-100 m-3  rounded-3xl" 
-                v-for="(subject, index) in sub" 
-                :key="index">
-                <button  
-                    class="h-12 m-3 rounded-full text-l relative -top-4 w-full text-center bg-gray-200 mx-auto">
+        
+            <div :class="(subject == 'PRA') ? 'col-span-3' : 'col-span-1'" class="bg-gray-100 m-3  rounded-3xl" v-for="(subject, index) in sub" :key="index">
+                <button  class="h-12 m-3 rounded-full text-l relative -top-4 w-full text-center bg-gray-200 mx-auto">
                     {{ subject }} 
                 </button>       
                 <div class="grid grid-cols-4"> 
-                    <button  
-                        v-for="module in modules" 
-                        v-show="module.subject == subject && module.period == p" 
-                        :key="module.id" 
-                        class="relative m-3 h-9 w-10/12 min-w-min bg-white text-start rounded-full"
-                        :class=" (module.subject === 'BIO') ? 'text-green-500  col-span-2 '
-                                        : (module.subject === 'CHE') ? 'text-teal-500 col-span-2  '
-                                        : (module.subject === 'PHY') ? 'text-sky-500 col-span-2  '
-                                        : (module.subject === 'MAT') ? 'text-red-400 col-span-2 '
-                                        : (module.subject === 'INT') ? 'text-indigo-400 col-span-2 '
-                                        : (module.subject === 'NEU') ? 'text-rose-300 col-span-2 '
-                                        :'text-slate-600 col-span-1'" 
-                        @click="makeChoice(module.id, module.subject, module.code, module.start1, module.end1, module.start2, module.end2, module.start3, module.end3, module.period)">
-                        <p class="font-bold h-16 mx-auto absolute left-0 top-0 text-s w-3/4 break-words text-leftinline">
-                             {{module.subject}}{{module.code}} 
-                        </p>
+                    <button  v-for="module in modules" v-show="module.subject == subject && module.period == p" :key="module.id" 
+                    class="relative m-3 h-9 w-10/12 min-w-min bg-white text-start rounded-full"
+                    :class=" (module.subject === 'BIO') ? 'text-green-500  col-span-2 '
+                                    : (module.subject === 'CHE') ? 'text-teal-500 col-span-2  '
+                                    : (module.subject === 'PHY') ? 'text-sky-500 col-span-2  '
+                                    : (module.subject === 'MAT') ? 'text-red-400 col-span-2 '
+                                    : (module.subject === 'INT') ? 'text-indigo-400 col-span-2 '
+                                    : (module.subject === 'NEU') ? 'text-rose-300 col-span-2 '
+                                    :'text-slate-600 col-span-1'" @click="makeChoice(module.id, module.subject, module.code, module.start1, module.end1, module.start2, module.end2, module.start3, module.end3, module.period)">
+                                    <p class="font-bold h-16 mx-auto absolute left-0 top-0 text-s w-3/4 break-words text-leftinline"> {{module.subject}}{{module.code}} </p>
                                     
-                        <span @mouseover="hover = true" @mouseleave="hover = false" class="w-1/4 h-9 block rounded-r-full absolute top-0 right-0 " :class="(module.subject === 'BIO') ? 'bg-green-500  col-span-1 row-span-1'
+                        <span class="w-1/4 h-9 block rounded-r-full absolute top-0 right-0 " :class="(module.subject === 'BIO') ? 'bg-green-500  col-span-1 row-span-1'
                                     : (module.subject === 'CHE') ? 'bg-teal-500'
                                     : (module.subject === 'PHY') ? 'bg-sky-500'
                                     : (module.subject === 'MAT') ? 'bg-red-400'
@@ -41,8 +29,7 @@
                                 : 'bg-slate-600'"> 
                             <a class="text-white text-xl text-bold justify-self-center absolute top-3" >i</a>
                         </span> 
-                    <Popup :hover="hover" />
-                </button>
+                    </button>
             </div> 
         </div>
     </div>
@@ -52,7 +39,6 @@
 <script>
 import Toggle from './Toggle.vue'
 import Choice from './Choice.vue'
-import Popup from './Popup.vue'
 
 export default {
     name: 'Semester',
@@ -63,13 +49,11 @@ export default {
     },
     components: {
         Toggle,
-        Choice,
-        Popup
+        Choice
     },
     data() {
         return {
             sub: ['BIO', 'CHE', 'PHY', 'INT', 'NEU', 'MAT', 'PRA'],
-            hover: false
         }
     },
     methods: {
